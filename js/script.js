44d05f613c47b0e571791e19869869d2d12b3b7f
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-15 16:53:26 +0000

    Finished 5-08

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Osmond
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  const numberA = parseInt(document.getElementById('number-a').value)
+  const numberB = parseInt(document.getElementById('number-b').value)
+  let remainder = numberA
+  let answer = 0
+
+  while (remainder > numberB) {
+    remainder -= numberB
+    answer++
+  }
+
+    if (remainder > 0) {
+    document.getElementById('answer').innerHTML = 'The answer is ' + answer + ' with a remainder of ' + remainder + "."
+  } else {
+  document.getElementById('answer').innerHTML = 'The answer is ' + answer + "." + ' There is no remainder.'
+  }
+}
